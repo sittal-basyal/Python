52c99f0aea26eb43d6d174221049bf27a96d9508
Sittal Basyal <bashyalshittal@gmail.com>
2025-05-31 11:44:23 +0545

    Create greatest.py

---
+++ greatest.py
@@ -0,0 +1,8 @@
+a=int(input("Enter a number:"))
+b=int(input("Enter 2nd number:"))
+c=int(input("Enter 3rd number:"))
+result=max(a,b,c)
+def max(a,b,c):
+    print(f"Greatest number between {a},{b},{c} is {result} " )
+
+max(a,b,c)
